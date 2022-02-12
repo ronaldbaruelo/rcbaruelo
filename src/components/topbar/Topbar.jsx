b1c6b406a98465ce0e94e89e62b58885536bb021
Ronald Baruelo <rcbaruelo10292@gmail.com>
2022-02-12 14:16:24 +1300

    intro

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import './Topbar.scss'
+import { Person, Mail } from '@material-ui/icons'
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={'topbar ' + (menuOpen && 'active')}>
+      <div className='wrapper'>
+        <div className='left'>
+          <a href='#intro' className='logo'>
+            dezigns.
+          </a>
+          <div className='itemContainer'>
+            <Person className='icon' />
+            <span>+64 275625535</span>
+          </div>
+          <div className='itemContainer'>
+            <Mail className='icon' />
+            <span>rcbaruelo1029@gmail.com</span>
+          </div>
+        </div>
+
+        <div className='right'>
+          <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
